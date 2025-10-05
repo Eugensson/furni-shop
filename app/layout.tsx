73d0d0d3c59b166f7117dd437fc4282323e1e2fb
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-10-05 11:51:27 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Saira } from "next/font/google";
+import "./globals.css";
+
+const saira = Saira({
+  variable: "--font-saira",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Modern Furniture for Every Home | FurniShop",
+    template: "%s | FurniShop",
+  },
+  description:
+    "Shop stylish and affordable furniture at FurniShop. Discover sofas, tables, chairs, beds, and more — designed to bring comfort and elegance to your home. Fast delivery and trusted quality.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${saira.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
